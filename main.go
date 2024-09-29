f1ff3514adf8e7a104560e5d0070dc7049774550
adiyakaihsan <adiyaka.ihsan@gmail.com>
2024-09-29 23:05:46 +0700

    implement channel for sending and processing job
    - use goroutine for http listener so it runs independently in the background while simultaneously we can loop channel to listen when a meessage is sent.

--- main.go
+++ main.go
@@ -1,30 +1,56 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
-	"io"
+	"log"
 	"net/http"
 
 	"github.com/julienschmidt/httprouter"
 )
 
+var channel = make(chan message)
+
+type message struct {
+	Title string `json:"title"`
+	Body  string `json:"body"`
+}
+
 func main() {
-	fmt.Printf("Hallo %v", "okokok")
+	fmt.Println("Ready to receive job!")
 	router := httprouter.New()
 
 	router.POST("/v1/api/jobs/submit", jobReceiver)
-	http.ListenAndServe(":8081", router)
-}
-
-func jobReceiver(w http.ResponseWriter, r * http.Request, _ httprouter.Params) {
-	body, err := io.ReadAll(r.Body)
-	if err != nil {
-		http.Error(w, "Error", http.StatusBadRequest)
+	go func() {
+		http.ListenAndServe(":8081", router)
+	}()
+	for msg := range channel {
+		jobProcessor(msg)
 	}
-	jobProcessor(string(body))
-	w.Write([]byte(body))
 }
 
-func jobProcessor(message string) {
-	fmt.Printf("Processing Job %v", message)
+func jobReceiver(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+	var msg message
+	if err := json.NewDecoder(r.Body).Decode(&msg); err != nil {
+		http.Error(w, "Unable to read request body", http.StatusBadRequest)
+		log.Printf("%s: %v", "Unable to read request body", err)
+		return
+	}
+
+	channel <- msg
+
+	w.Write([]byte("Job is sent"))
 }
+
+func jobProcessor(msg message) {
+	log.Printf("====================================")
+	log.Printf("Title: %s", msg.Title)
+	log.Printf("Body: %s", msg.Body)
+	log.Printf("====================================")
+}
+
+/*
+Sample request:
+curl localhost:8081/v1/api/jobs/submit -d '{"title":"P1", "body":"trigger alert"}'
+
+*/
